f0fd990d845fd97d66e2a6dd0164089c27bad443
Arthur Vincent Simon <developarvin@gmail.com>
2020-12-16 21:33:46 +0800

    Create initial SQL

--- init.sql
+++ init.sql
@@ -1,6 +1,8 @@
 SELECT DATABASE "postgres"
 
 # Creating Tables
+START TRANSACTION;
+DROP TABLE IF EXISTS players;
 CREATE TABLE players
 (
     id   serial not null
@@ -9,6 +11,7 @@ CREATE TABLE players
     name text
 );
 
+DROP TABLE IF EXISTS scores;
 CREATE TABLE scores
 (
     id           serial not null
@@ -20,5 +23,34 @@ CREATE TABLE scores
             references players,
     date_created timestamp
 );
+END TRANSACTION;
 
-#
+# Insert data
+START TRANSACTION;
+INSERT INTO players(name)
+VALUES ("John")
+RETURNING id  AS p_id;
+
+INSERT INTO scores(score, player_id, date_created)
+VALUES (111, p_id,1607751459)
+
+INSERT INTO scores(score, player_id, date_created)
+VALUES (122, p_id,1607837857)
+
+INSERT INTO scores(score, player_id, date_created)
+VALUES (133, p_id,1607924257)
+
+INSERT INTO players(name)
+VALUES ("Jane")
+RETURNING id AS p_id;
+
+INSERT INTO scores(score, player_id, date_created)
+VALUES (211, p_id,1607751459)
+
+INSERT INTO scores(score, player_id, date_created)
+VALUES (222, p_id,1607837857)
+
+INSERT INTO scores(score, player_id, date_created)
+VALUES (233, p_id,1607924257)
+END TRANSACTION;
+
