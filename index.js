82b9c56f4318f1d032eeb7e37a567bab0b87ace2
Arthur Vincent Simon <developarvin@gmail.com>
2021-02-10 21:59:05 +0800

    Create initial Apollo Server setup

---
+++ index.js
@@ -0,0 +1,45 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+    type Player {
+        id: ID
+        name: String
+    }
+
+    type Query {
+        players: [Player]
+    }
+`;
+
+const players = [
+    {
+        id: 1,
+        name: "John Doe",
+    },
+    {
+        id: 2,
+        name: "Jane Doe",
+    },
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+        players: () => players,
+    },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen({
+    port: 1234
+}).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
